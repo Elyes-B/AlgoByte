63aeed43243798d3c6ac3829f5d30e23f09b13a4
camiouna <hachani.0123@gmail.com>
2026-03-26 14:22:59 +0100

    front and back with ide execution

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import HeaderBar from './components/HeaderBar.vue'
-import FooterBar from './components/FooterBar.vue';
+import FooterBar from './components/FooterBar.vue'
+import editorComponent from './components/editorComponent.vue';
 </script>
 
 <template>
   <HeaderBar />
   <router-view />
+  <editorComponent></editorComponent>
   <FooterBar />
 </template>
