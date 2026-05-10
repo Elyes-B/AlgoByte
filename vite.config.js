af018d3db79d0ef72ea8da799726023bdaff517c
Ilyes Belkahia <elyesbelkahia22@gmail.com>
2026-05-10 10:24:31 +0100

    migrate from vue only to laravel

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    proxy: {
-      '/piston': {
-        target: 'http://localhost:2000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/piston/, '')
-      }
-    }
-  }
-})
+    plugins: [
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+});
